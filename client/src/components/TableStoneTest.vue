98d9dd826cac756e1948511b967e7e07f1ed2a3a
Leonid Varygin <leonid.warygin@gmail.com>
2021-02-23 20:34:09 +0700

    calc sieves values

--- client/src/components/TableStoneTest.vue
+++ client/src/components/TableStoneTest.vue
@@ -2,21 +2,25 @@
   <div class="startWeight">
     <label for="startWeight">Начальная масса</label>
     <br>
-    <input type="number" id="startWeight">
+    <input type="number" id="startWeight" v-model="startWeight">
   </div>
   <div>
     <table v-for="sieve of sieves" :key="sieve">
       <thead></thead>
       <tr>
-        <th v-for="diamether of sieve">{{ diamether }}</th>
+        <th v-for="diameter of sieve">{{ diameter }}</th>
       </tr>
 
       <tbody>
-      <tr >
-        <td v-for="(row,i) in sieve" :key="row"><input type="text"></td>
+      <tr>
+        <td v-for="(row, i) in sieve">
+          <input type="number" v-model.number="inputData[i]" @blur="click">
+        </td>
       </tr>
       </tbody>
     </table>
+
+    <pre>{{ JSON.stringify(name, null, 2) }}</pre>
   </div>
 </template>
 
@@ -30,13 +34,41 @@ export default {
   setup() {
     const store = useStore()
     const sieves = computed(() => store.getters['generateSieves/sieves'])
-    const p = ref(sieves)
-    console.log(p)
-    return {sieves}
+    const name = ref(name)
+    const startWeight = 5000
+    const inputData = {}
+    const result = []
+
+
+    const click = ($event) => {
+      // console.log($event.target.value)
+      // result.push((Object.values(inputData).reduce((a, b) => a / b, 100)))
+      // const res = (Object.values(inputData) / startWeight) * 100
+      // console.log(res)
+      console.log(Object.values(inputData).map(el => (el / startWeight) * 100))
+      result.push(Object.values(inputData).reduce((a, b) => a + b, 0))
+      console.log(result)
+    }
+
+    const calc = computed(() => {
+
+    })
+
+    return {
+      sieves,
+      name,
+      click,
+      calc,
+      inputData,
+      startWeight,
+      result
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-
+input {
+  max-width: 80px;
+}
 </style>
